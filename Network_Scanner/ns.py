303ac2a17046103120889e8ab7b9bbfe881bed2a
Alkım Doğan <121884935+doganalkim@users.noreply.github.com>
2024-03-22 13:50:45 +0300

    Network Scanner Added

---
+++ Network_Scanner/ns.py
@@ -0,0 +1,40 @@
+import optparse
+import scapy.all as scapy
+
+
+def command_input():
+    parser = optparse.OptionParser()
+    parser.add_option("-r","--range",dest = "iprange",help = "Enter your IP range")
+    (o,a) = parser.parse_args()
+    print(type(o.iprange))
+    return o.iprange
+def scan(ip):
+    #scapy.arping(ip) # We will implement it manually
+    arp_request = scapy.ARP(pdst = ip)
+    #arp_request.show()
+    #print(arp_request.summary()) # prints the summary
+    #scapy.ls(scapy.ARP()) # prints the variables to set
+    broadcast = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
+    #scapy.ls(scapy.Ether())
+    #print(broadcast.summary())
+    #broadcast.show()
+    arp_request_broadcast = broadcast/arp_request
+    #print(arp_request_broadcast.summary())
+    #arp_request_broadcast.show()
+    ansed, unansed = scapy.srp(arp_request_broadcast,timeout = 1,verbose = False)
+    #print(ansed.summary())
+    #print(unansed.summary())
+
+    print("IP\t\tMAC ADRESS")
+    client_list = []
+    for i in ansed:
+        client = {"ip":i[i].psrc, "MAC":i[1].hwsrc}
+        client_list.append(client)
+        print(i[1].psrc, end = "\t")
+        print(i[1].hwsrc)
+        print("--------------")
+
+
+ip = command_input()
+scan(ip)
+
