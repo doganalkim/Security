7f21f7e9fb69ecdfb55f9f248facf884f12772cf
Alkım Doğan <121884935+doganalkim@users.noreply.github.com>
2024-06-15 10:32:13 +0300

    comments deleted

--- Network_Scanner/ns.py
+++ Network_Scanner/ns.py
@@ -7,22 +7,12 @@ def command_input():
     parser.add_option("-r","--range",dest = "iprange",help = "Enter your IP range")     # Add range as a command line arguement
     (o,a) = parser.parse_args()
     return o.iprange
+
 def scan(ip):
-    #scapy.arping(ip) # We will implement it manually
     arp_request = scapy.ARP(pdst = ip)
-    #arp_request.show()
-    #print(arp_request.summary()) # prints the summary
-    #scapy.ls(scapy.ARP()) # prints the variables to set
     broadcast = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
-    #scapy.ls(scapy.Ether())
-    #print(broadcast.summary())
-    #broadcast.show()
     arp_request_broadcast = broadcast/arp_request
-    #print(arp_request_broadcast.summary())
-    #arp_request_broadcast.show()
     ansed, unansed = scapy.srp(arp_request_broadcast,timeout = 1,verbose = False)
-    #print(ansed.summary())
-    #print(unansed.summary())
 
     print("IP\t\tMAC ADRESS")
     client_list = []
